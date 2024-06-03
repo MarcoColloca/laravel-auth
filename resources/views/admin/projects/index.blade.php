f07e8d0bd9cb7f8d4a7f17994d8a2b6471433cec
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-03 16:56:59 +0200

    Add → projects Index MVC

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+    
+    <section class="mb-5 py-5 bg-purple">
+        <div class="bg-light container py-4 projects-cotnainer">
+
+            <h1 class="my-4 text-center"> My Projects</h1>
+
+            <table class="table table-dark table-stripped table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Date of Creation</th>
+                        <th scope="col">Type of Project</th>
+                        <th scope="col">Contributors</th>
+                        <th scope="col">More Info</th>
+                        <th scope="col">Modify</th>
+                        <th scope="col">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($projects as $project)                
+                        <tr>
+                            <td>{{$project->name}}</td>
+                            <td>{{$project->date_of_creation}}</td>
+                            <td>{{$project->is_public === 0 ? 'Public' : 'Private'}}</td>
+                            <td>{{$project->contributors}}</td>
+                            <td><a class="text-success" href="">Show Info</a></td>
+                            <td><a class="text-primary" href="">Edit</a></td>
+                            <td><a class="text-danger text-center" href="">X</a></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </section>
+    
+@endsection
