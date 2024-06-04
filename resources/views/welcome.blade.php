b80711cd685f9212e0b9702ed2bed911dcfa05d3
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-04 09:38:35 +0200

    Add →changed style to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="jumbotron p-5 mb-4 bg-light rounded-3">
+<div class="jumbotron p-5 mb-4 text-light rounded-3">
     <div class="container py-5">
         <div class="logo_laravel">
             <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-25">
@@ -11,20 +11,12 @@
             </svg>
         </div>
         <h1 class="display-5 fw-bold">
-            Welcome to Laravel+Bootstrap <i class="bi bi-box"></i>
+            Welcome to my First Laravel Auth Project <i class="bi bi-box"></i>
         </h1>
 
         <p class="col-md-8 fs-4">
-            This is a preset package with Bootstrap 5 views for laravel projects including laravel breeze/blade. It works from laravel 9.x to the latest release 11.x.
-            You can also use bootstrap icons out of the box.
+            This project has the goal to wrap all the Git Hub projects i made during my Boolean Course. And it is a way to learn the Authorization approach and methods that Laravel puts at our disposal.
         </p>
-        <a href="https://packagist.org/packages/pacificdev/laravel_9_preset" class="btn btn-primary btn-lg" type="button">Documentation</a>
-    </div>
-</div>
-
-<div class="content">
-    <div class="container">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis accusamus dolores!</p>
     </div>
 </div>
 @endsection
